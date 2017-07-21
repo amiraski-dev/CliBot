eebf8b188e03789df33b0810d657282582946dcb
somerhalder-hs <30274528+somerhalder-hs@users.noreply.github.com>
2017-07-21 16:46:45 +0000

    Update music.lua

--- gml/music.lua
+++ gml/music.lua
@@ -1,9 +1,9 @@
 --@somerhalder
 --t.me/sourcesiran
 local function run(msg, matches)
-local bot_music = 117678843
-if matches[1]:lower() == 'music' then
-local function musicbot(arg, data)
+local bot_like = 190601014
+if matches[1]:lower() == 'like' then
+local function likebot(arg, data)
 if data.results_ and data.results_[0] then
 tdcli.sendInlineQueryResultMessage(msg.chat_id_, msg.id_, 0, 1, data.inline_query_id_, data.results_[0].id_)
 del_msg(msg.to.id, msg.id)
@@ -12,13 +12,13 @@ tdcli.sendMessage(msg.chat_id_, msg.id_, 0, 1, nil, '*>|:  : (*\n>*لطفا دو
 del_msg(msg.to.id, msg.id)
 end
 end
-tdcli.getInlineQueryResults(bot_music, msg.chat_id_, 0, 0, matches[2], 0, musicbot, nil)
+tdcli.getInlineQueryResults(bot_like, msg.chat_id_, 0, 0, matches[2], 0, likebot, nil)
 del_msg(msg.to.id, msg.id)
 end
 end
 return {
 patterns = {
-"^(music) (.*)"
+"^(like) (.*)"
 },
 run = run,
 }
