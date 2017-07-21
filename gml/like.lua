e94c3d575836d706a0d6f8d3aefee16bf2fcd785
somerhalder-hs <30274528+somerhalder-hs@users.noreply.github.com>
2017-07-21 15:22:14 +0000

    @like

--- gml/like.lua
+++ gml/like.lua
@@ -5,7 +5,7 @@ local bot_like = 190601014
 if matches[1]:lower() == 'like then
 local function musicbot(arg, data)
 if data.results_ and data.results_[0] then
-tdcli.sendInlineQuerResultMessage(msg.chat_id_, msg.id_, 0, 1, data.inline_query_id_, data.results_[0].id_
+tdcli.sendInlineQueryResultMessage(msg.chat_id_, msg.id_, 0, 1, data.inline_qury_id_, data.results_[0].id_
 del_msg(msg.to.id, msg.id)
 else
 tdcli.sendMessage(msg.chat_id_, msg.id_, 0, 1, nil, '*>|:  : (*\n>*لطفا دوباره تلاش کنید : )*', 1, 'md')
